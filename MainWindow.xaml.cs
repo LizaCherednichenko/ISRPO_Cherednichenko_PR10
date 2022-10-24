64fe0724393dc3cb430ed5c554de4978da3388c3
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-10-24 12:02:01 +1000

    удаление

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,5 +29,10 @@ namespace ISRPO_Cherednichenko_PR10
         {
             listBox1.Items.Add(textbox1.Text);
         }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
     }
 }
