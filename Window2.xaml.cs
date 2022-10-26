ceecf5849873f298ee2a533c8495ad8d591c8850
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-10-26 13:53:54 +1000

    готовое

--- Window2.xaml.cs
+++ Window2.xaml.cs
@@ -23,5 +23,45 @@ namespace ISRPO_Cherednichenko_PR10
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            listbox1.Items.Add(textbox1.Text);
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            listbox1.Items.Remove(listbox1.SelectedItem);
+        }
+
+        private void Sort_Click(object sender, RoutedEventArgs e)
+        {
+            listbox1.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Content", System.ComponentModel.ListSortDirection.Ascending));
+        }
+
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            image1.Source = new BitmapImage(new Uri("image/1.jpg", UriKind.Relative));
+        }
+
+        private void ListBoxItem_Selected_1(object sender, RoutedEventArgs e)
+        {
+            image1.Source = new BitmapImage(new Uri("image/2.jpg", UriKind.Relative));
+        }
+
+        private void ListBoxItem_Selected_2(object sender, RoutedEventArgs e)
+        {
+            image1.Source = new BitmapImage(new Uri("image/3.jpg", UriKind.Relative));
+        }
+
+        private void ListBoxItem_Selected_3(object sender, RoutedEventArgs e)
+        {
+            image1.Source = new BitmapImage(new Uri("image/4.jpg", UriKind.Relative));
+        }
+
+        private void ListBoxItem_Selected_4(object sender, RoutedEventArgs e)
+        {
+            image1.Source = new BitmapImage(new Uri("image/5.jpg", UriKind.Relative));
+        }
     }
 }
